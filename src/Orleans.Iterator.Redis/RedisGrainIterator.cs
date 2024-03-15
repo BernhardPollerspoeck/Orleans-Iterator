6623ef3bb1440281ff5b4c3ea0735eebf622b273
Bernhard Pollerspöck <b.pollerspoeck@bytecookie.de>
2024-03-15 19:45:57 +0100

    Reviewed

--- src/Orleans.Iterator.Redis/RedisGrainIterator.cs
+++ src/Orleans.Iterator.Redis/RedisGrainIterator.cs
@@ -7,25 +7,14 @@ using Orleans.Iterator.Abstraction.Server;
 
 namespace Orleans.Iterator.Redis;
 
-public class RedisGrainIterator : IServerGrainIterator
+public class RedisGrainIterator(IServiceProvider serviceProvider) : IServerGrainIterator
 {
-    #region fields
-    private readonly IServiceProvider _serviceProvider;
-    #endregion
-
-    #region ctor
-    public RedisGrainIterator(IServiceProvider serviceProvider)
+	#region IServerGrainIterator
+	public Task<IIterativeServerGrainReader> GetReader<TGrainInterface>(params GrainDescriptor[] grainDescriptions) where TGrainInterface : IGrain
     {
-        _serviceProvider = serviceProvider;
-    }
-    #endregion
-
-    #region IServerGrainIterator
-    public Task<IIterativeServerGrainReader> GetReader<TGrainInterface>(params GrainDescriptor[] grainDescriptions) where TGrainInterface : IGrain
-    {
-        var storageOptions = _serviceProvider.GetRequiredService<IOptions<RedisGrainIteratorOptions>>();
-        var clusterOptions = _serviceProvider.GetRequiredService<IOptions<ClusterOptions>>();
-        var logger = _serviceProvider.GetRequiredService<ILogger<RedisIterativeGrainReader<TGrainInterface>>>();
+        var storageOptions = serviceProvider.GetRequiredService<IOptions<RedisGrainIteratorOptions>>();
+        var clusterOptions = serviceProvider.GetRequiredService<IOptions<ClusterOptions>>();
+        var logger = serviceProvider.GetRequiredService<ILogger<RedisIterativeGrainReader<TGrainInterface>>>();
         return Task.FromResult(
             (IIterativeServerGrainReader)new RedisIterativeGrainReader<TGrainInterface>(
                 storageOptions,
